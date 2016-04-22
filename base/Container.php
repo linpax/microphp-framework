3a06f03dc027ec12d5f33dc74dd53c097024c94e
casper <antivir88@mail.ru>
2016-04-22 12:33:17 +0600

    update DI

--- base/Container.php
+++ base/Container.php
@@ -120,8 +120,8 @@ class Container extends \stdClass implements IContainer
         }
 
         if ($name === null) {
-            foreach ($this->components AS $name => $options) {
-                if (!$this->loadComponent($name, $options)) {
+            foreach ($this->components AS $key => $options) {
+                if (!$this->loadComponent($key, $options)) {
                     return false;
                 }
             }
@@ -155,17 +155,101 @@ class Container extends \stdClass implements IContainer
         if (empty($options['class']) || !class_exists($options['class'])) {
             return false;
         }
+
         $className = $options['class'];
+        $this->data[$name] = null;
 
-        if (empty($options['arguments'])) {
-            $this->data[$name] = new $className;
+        $options['arguments'] = !empty($options['arguments']) ? $this->buildParams($options['arguments']) : null;
+        $options['property'] = !empty($options['property']) ? $this->buildParams($options['property']) : null;
+        $options['calls'] = !empty($options['calls']) ? $this->buildCalls($options['calls']) : null;
 
-            return true;
+        try { // create object
+            $reflection = new \ReflectionClass($className);
+            $reflectionMethod = new \ReflectionMethod($className, '__construct');
+
+            if ($reflectionMethod->getNumberOfParameters() === 0) {
+                $this->data[$name] = new $className;
+            } else {
+                $this->data[$name] = $reflection->newInstanceArgs($options['arguments']);
+            }
+
+            unset($reflection, $reflectionMethod);
+        } catch (Exception $e) {
+            return false;
         }
 
+        if (!empty($options['property'])) { // load properties
+            foreach ($options['property'] as $key => $val) {
+                if (property_exists($this->data[$name], $key)) {
+                    $this->data[$name]->$key = $val;
+                }
+            }
+        }
+
+        if (!empty($options['calls'])) { // run methods
+            foreach ($options['calls'] as $key => $val) {
+                if (method_exists($this->data['name'], $key)) {
+                    $reflectionMethod = new \ReflectionMethod($className, $key);
+                    if ($reflectionMethod->getNumberOfParameters() === 0) {
+                        $this->data['name']->$key();
+                    } else {
+                        call_user_func_array([$this->data['name'], $key], $val);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Build calls arguments
+     *
+     * @access private
+     * @param array $params
+     * @return array
+     */
+    private function buildCalls(array $params)
+    {
+        $calls = [];
+
+        if (!is_array($params[0])) {
+            $call[] = $params[0];
+            unset($params[0]);
+
+            if (!empty($params[1])) {
+                $call[] = $params[1];
+                unset($params[1]);
+            }
+
+            $params[] = $call;
+        }
+
+        foreach ($params as $arguments) {
+            if (is_string($arguments[0])) {
+                if (!empty($arguments[1])) {
+                    $calls[$arguments[0]] = $this->buildParams($arguments[1]);
+                } else {
+                    $calls[$arguments[0]] = null;
+                }
+            }
+        }
+
+        return $calls;
+    }
+
+    /**
+     * Build params from array
+     *
+     * @access private
+     * @param array $params
+     * @return array
+     */
+    private function buildParams(array $params)
+    {
         /** @noinspection AlterInForeachInspection */
-        foreach ($options['arguments'] AS $key => &$val) {
-            if (is_string($options['arguments'][$key]) && (0 === strpos($val, '@'))) {
+        foreach ($params AS $key => &$val) { // IoC Constructor
+            if (is_string($params[$key]) && (0 === strpos($val, '@'))) {
                 if ($val === '@this') {
                     $val = $this;
                 } else {
@@ -176,8 +260,7 @@ class Container extends \stdClass implements IContainer
                 }
             }
         }
-        $this->data[$name] = new $className($options['arguments']);
 
-        return true;
+        return $params;
     }
 }
