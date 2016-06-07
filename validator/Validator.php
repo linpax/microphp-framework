ad37dbeb631f70765540cd9b8b705c4ca0000a87
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-06-07 16:06:42 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- validator/Validator.php
+++ validator/Validator.php
@@ -126,7 +126,7 @@ class Validator
             return '\\Micro\\Validator\\'.self::$validators[$name];
         } elseif (class_exists($name) && is_subclass_of($name, '\Micro\Validator\IValidator')) {
             return $name;
-        } elseif (file_exists((new KernelInjector)->build()->getAppDir() . '/validator/' . $name . '.php')) {
+        } elseif (file_exists((new KernelInjector)->build()->getAppDir().'/validator/'.$name.'.php')) {
             return '\\App\\Validator\\'.$name;
         }
 
