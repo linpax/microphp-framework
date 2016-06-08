17d38b9792e29f139336ae830832453a3780db85
Zcasper <testuser@mail.linpax.org>
2016-06-08 10:52:23 +0600

    flash injector

---
+++ web/FlashInjector.php
@@ -0,0 +1,36 @@
+<?php /** MicroFlashInjector */
+
+namespace Micro\Web;
+
+use Micro\Base\Exception;
+use Micro\Base\Injector;
+
+/**
+ * Class FlashInjector
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/linpax/microphp-framework
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license /LICENSE
+ * @package Micro
+ * @subpackage Web
+ * @version 1.0
+ * @since 1.0
+ */
+class FlashInjector extends Injector
+{
+    /**
+     * @return FlashMessage
+     * @throws Exception
+     */
+    public function build()
+    {
+        $flash = $this->get('flash');
+
+        if (!($flash instanceof FlashMessage)) {
+            throw new Exception('Component `flash` not configured');
+        }
+
+        return $flash;
+    }
+}
